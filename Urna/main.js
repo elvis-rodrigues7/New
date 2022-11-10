8e524531112f3fabb838881b1978165760da54a8
elvis-rodrigues7 <116103185+elvis-rodrigues7@users.noreply.github.com>
2022-11-10 16:51:38 -0300

    Add files via upload

--- Urna/main.js
+++ Urna/main.js
@@ -1,9 +1,64 @@
-var listaNumero = document.querySelectorAll('.num__style');
-var primeiroNumero = document.querySelector('.num1__resultado')
+function inserir(valor) {
+    var primeiroTermo = document.querySelector('#campo1').value;
+    var segundoTermo = document.querySelector('#campo2').value;
 
+    if (primeiroTermo == "") {
+        document.querySelector('#campo1').value = valor;
+    }
 
-listaNumero[0].addEventListener('click', atribuindoValor1)
+    else if (segundoTermo == "") {
+        document.querySelector('#campo2').value = valor;
+    }
+}
 
-function atribuindoValor1() {
-    primeiroNumero.textContent = '1';
+function branco(){
+    document.querySelector('#campo1').value = "0";
+    document.querySelector('#campo2').value = "0";
+}
+
+function corrigir(){
+    document.querySelector('#campo1').value = "";
+    document.querySelector('#campo2').value = "";
+}
+
+function votar() {
+    var primeiroTermo = parseInt(document.querySelector('#campo1').value)
+    var segundoTermo = parseInt(document.querySelector('#campo2').value)
+    var candidato = (primeiroTermo * 10) + segundoTermo;
+    if (sessionStorage.getItem(candidato) !== null) {
+        votos = parseInt(sessionStorage.getItem(candidato)) + 1;
+        sessionStorage.setItem(candidato, votos);
+        fim()
+        setTimeout(recomeço, 2000)
+    } else {
+        sessionStorage.setItem(candidato, 1);
+        fim()
+        setTimeout(recomeço, 2000)
+    }
+    document.querySelector('#campo1').value = "";
+    document.querySelector('#campo2').value = "";
+}
+
+function fim(){
+    var presidenteInvisivel = document.querySelector('.visu__presi')
+    var primeiroTermoInvisivel = document.querySelector('.num1__resultado')
+    var segundoTermoInvisivel = document.querySelector('.num2__resultado')
+    var fimVisivel = document.querySelector('.voto__confirmado')
+
+    presidenteInvisivel.classList.add("inv")
+    primeiroTermoInvisivel.classList.add("inv")
+    segundoTermoInvisivel.classList.add("inv")
+    fimVisivel.classList.remove("inv")
+}
+
+function recomeço() {
+    var presidenteInvisivel = document.querySelector('.visu__presi')
+    var primeiroTermoInvisivel = document.querySelector('.num1__resultado')
+    var segundoTermoInvisivel = document.querySelector('.num2__resultado')
+    var fimVisivel = document.querySelector('.voto__confirmado')
+
+    presidenteInvisivel.classList.remove("inv")
+    primeiroTermoInvisivel.classList.remove("inv")
+    segundoTermoInvisivel.classList.remove("inv")
+    fimVisivel.classList.add("inv")
 }
