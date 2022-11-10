3f7952bce59e509d16368b5b91955ef4a512a50a
elvis-rodrigues7 <116103185+elvis-rodrigues7@users.noreply.github.com>
2022-11-10 11:31:31 -0300

    Delete New-main/Urna directory

--- New-main/Urna/main.js
+++
@@ -1,9 +0,0 @@
-var listaNumero = document.querySelectorAll('.num__style');
-var primeiroNumero = document.querySelector('.num1__resultado')
-
-
-listaNumero[0].addEventListener('click', atribuindoValor1)
-
-function atribuindoValor1() {
-    primeiroNumero.textContent = '1';
-}
